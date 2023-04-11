fd61c8eb6f651a7f73b187a07c124a5052524f9c
XY-ShadowK <1714155654@qq.com>
2023-04-11 23:54:28 +0800

    the origin start of this aiml chatbot

---
+++ src/aiml chatbot/aiml chatbot.py
@@ -0,0 +1,34 @@
+import aiml
+import os
+
+'''
+def get_module_dir(name):
+    path=getattr(sys.modules[name],'__file__',None)
+    if not path:
+        raise AttributeError('module %s has not attribute __file__' %name)
+    return os.path.dirname(os.path.abspath(path))
+
+alice_path=get_module_dir('aiml')+'/alice'
+os.chdir(alice_path)
+
+alice=aiml.Kernel()
+alice.learn(startup.xml)
+alice.respond('LOAD ALICE')
+
+while True:
+    print alice.respond(raw_input("Enter your message >> "))
+'''
+
+chatbot_path = '../../chatbot/corpus'
+os.chdir(chatbot_path)
+
+chatbot = aiml.Kernel()
+chatbot.learn("std-startup.xml")
+
+print("choosing the aiml chatbot!")
+print("1. basic chatbot")
+if input() == "1":
+    chatbot.respond('load aiml basic')
+
+while True:
+    print(chatbot.respond(input("Enter your message >> ")))
